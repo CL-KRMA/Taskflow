ba45f1fa4330eb5cd794879fed2316ead29550ff
cl-krma <clkourouma688@gmail.com>
2026-03-27 01:28:47 +0000

    update galerie en ligne

--- components/Accueil.tsx
+++ components/Accueil.tsx
@@ -9,7 +9,7 @@ const Accueil = () => {
             Ici, vous découvrirez une galerie dynamique qui met en valeur des moments, des paysages et des créations uniques.<br />
             Notre objectif est simple: offrir une expérience immersive où l’esthétique rencontre la simplicité. <br />
             Explorez, contemplez et laissez-vous inspirer par la diversité des visuels présentés.<br />
-            Ce site est conçu pour être votre fenêtre ouverte sur la beauté du monde, directement depuis votre écran.
+            Ce site est conçu pour être votre fenêtre ouverte sur la beauté du monde, directement depuis votre écran.ok
         </p>
     </div>
   )
