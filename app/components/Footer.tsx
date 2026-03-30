4a8f92590e87d7dcc1f3f3bac26c480e2510af9b
cl-krma <clkourouma688@gmail.com>
2026-03-30 23:24:14 +0000

    deployement task flow

--- app/components/Footer.tsx
+++ app/components/Footer.tsx
@@ -1,7 +1,7 @@
 export default function Footer() {
   return (
     <footer className="footer">
-      <p>&copy; 2026 TaskFlow. Tous droits réservés.</p>
+      <p>&copy; 2026 Cheick L. Dev. TaskFlow. Tous droits réservés.</p>
       <p>Votre partenaire de gestion de projets en ligne</p>
     </footer>
   );
